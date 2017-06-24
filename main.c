1f55531630851b86eec88654affccd20747a13c4
NP <nb.piscitello@gmail.com>
2017-06-23 20:00:09 -0400

    added sliding transition

--- main.c
+++ main.c
@@ -27,6 +27,9 @@ const __flash uint64_t icons[] =
   0xaa55aa55aa55aa55,   // fog
 };
 
+// YCM will complain about this - it uses a GCC feature not implemented in clang
+const __flash uint64_t blank_screen = 0x0000000000000000;
+
 // direct links to specific icons (IDK if they'll be useful, but might as well provide them)
 #define SUN       0
 #define MOON      1
@@ -54,15 +57,35 @@ void transmit(const uint8_t reg, const uint8_t val) {
   while( !(SPSR & _BV(SPIF)) ) {}
   // bring CS high
   PORTB |= 0x00 | _BV(PORTB2);
+  return;
 }
 
 // update the entire matrix from a 64-bit number
 void update_screen(const uint64_t icon) {
   // transmit 8 rows at a time. We're taking advantage of the fact that 
   // the display registers start at 1 on the MAX72XX
+  // treat the icon like a uint8_t array
   for( uint8_t i = 0x01; i < 0x09; i++ ) {
-    transmit(i, (icon >> (i - 1) * 8) & 0xFF);
+    transmit(i, ((uint8_t*)&icon)[i - 1]);
   }
+  return;
+}
+
+/* slide transition between icons
+ *  out_icon: old icon to be replaced
+ *  in_icon:  new icon to be displayed
+ *  space:    number of blank columns between icons */
+void slide_transition(const uint64_t out_icon, const uint64_t in_icon, const uint8_t space) {
+  // index across frames
+  for( uint8_t i = 1; i <= 8 + space; i++ ) {
+    // modified update_screen - treat the icon like an 8-member uint8_t array
+    // the shifts are 'backwards'; this is due to the writes being from bottom to top
+    for( uint8_t j = 0x01; j < 0x09; j++ ) {
+      transmit(j, ((((uint8_t*)&out_icon)[j - 1]) >> i) | ((((uint8_t*)&in_icon)[j - 1]) << (8 - i + space)));
+    }
+    _delay_ms(150);
+  }
+  return;
 }
 
 int main(void) {
@@ -89,10 +112,18 @@ int main(void) {
   // take the chip out of shutdown mode
   transmit(0x0C, 0x01);
 
+  // slide the first icon onto the screen
+  slide_transition(blank_screen, icons[FIRST], 0);
+  _delay_ms(1000);
+
   // roll through the defined icons
   while( 1 == 1 ) {
     for( uint8_t i = FIRST; i <= LAST; i++ ) {
-      update_screen(icons[i]);
+      if( i != LAST ) {
+        slide_transition(icons[i], icons[i+1], 2);
+      } else {
+        slide_transition(icons[LAST], icons[FIRST], 2);
+      }
       _delay_ms(1000);
     }
   }
