a869bb79f9f05a2e2305ddba7f5ec52d6c4bd773
NP <nb.piscitello@gmail.com>
2017-06-25 14:59:44 -0400

    separated graphics.h from animation.h

--- animation.h
+++ animation.h
@@ -3,7 +3,6 @@
 
 #include <avr/io.h>
 #include <stdint.h>
-#include "graphics.h"
 
 // data for slide transition
 struct transition_data {
@@ -17,7 +16,9 @@ struct transition_data {
   uint8_t   frame_ready :1; // true if the inter-frame time has elapsed
   uint8_t   bool6       :1;
   uint8_t   bool7       :1;
-} adata;
+};
+typedef struct transition_data transition_data;
+
 // these aren't defined
 #define false 0
 #define true  1
@@ -29,6 +30,8 @@ struct transition_data {
 // don't want to make this file dependent on the graphics header
 #define ICON_W  8
 #define NUM_W   6
+
+
 
 // update a register on the MAX72XX
 void transmit(const uint8_t reg, const uint8_t val) {
@@ -49,81 +52,89 @@ void transmit(const uint8_t reg, const uint8_t val) {
   return;
 }
 
+
+
 // this does all the work - it is to be called in a loop to generate the transition
-void transition_loop(void) {
+void transition_loop(transition_data* tdata) {
   TIFR0 |= _BV(OCF0A);
-  adata.frame++;
+  tdata->frame++;
   
   /* we need three if statements because, with just one, the arguments to the left shifts would become
    * negative. This could probably be mitigated with some macro magic, but I am no magician.
    * Besides, I find this way clearer. */
 
   // different loops depending on if we're transitioning between icons or a set of numbers.
-  if( adata.icon ) {
-    if( adata.frame <= adata.space + ICON_W ) {
+  if( tdata->icon ) {
+    if( tdata->frame <= tdata->space + ICON_W ) {
       // treat the icon like an 8-member uint8_t array
       // the shifts are 'backwards'; this is due to the writes being from right to left
       for( uint8_t j = 0x01; j < 0x09; j++ ) {
-        transmit(j, ((((uint8_t*)&adata.out_icon)[j - 1]) >> adata.frame) | 
-            ((((uint8_t*)&adata.in_icon)[j - 1]) << (adata.space + ICON_W - adata.frame)));
+        transmit(j, ((((uint8_t*)&tdata->out_icon)[j - 1]) >> tdata->frame) | 
+            ((((uint8_t*)&tdata->in_icon)[j - 1]) << (tdata->space + ICON_W - tdata->frame)));
       }
 
     } else {
       // this means we are at our last frame
       TCCR0B &= (0xFF & !_BV(CS02) & !_BV(CS00));
-      adata.trans_done = true;
+      tdata->trans_done = true;
     }
 
   } else {
     // wipe across the first space + ICON_W columns (clear icon)
-    if( adata.frame <= adata.space + ICON_W ) {
+    if( tdata->frame <= tdata->space + ICON_W ) {
       for( uint8_t j = 0x01; j < 0x09; j++ ) {
-        transmit(j, ((((uint8_t*)&adata.out_icon)[j - 1]) >> adata.frame) | 
-            ((((uint8_t*)&adata.in_icon[0])[j - 1]) << (adata.space + ICON_W - adata.frame)) |
-            ((((uint8_t*)&adata.in_icon[1])[j - 1]) << (adata.space + ICON_W + NUM_W - adata.frame)));
+        transmit(j, ((((uint8_t*)&tdata->out_icon)[j - 1]) >> tdata->frame) | 
+            ((((uint8_t*)&tdata->in_icon[0])[j - 1]) << (tdata->space + ICON_W - tdata->frame)) |
+            ((((uint8_t*)&tdata->in_icon[1])[j - 1]) << (tdata->space + ICON_W + NUM_W - tdata->frame)));
       }
 
-    } else if( adata.frame <= adata.space + ICON_W + NUM_W
-        && adata.frame > adata.space + ICON_W
-        && adata.number > 9) {
+    } else if( tdata->frame <= tdata->space + ICON_W + NUM_W
+        && tdata->frame > tdata->space + ICON_W
+        && tdata->number > 9) {
       // wipe across the next NUM_W columns (clear first digit)
       for( uint8_t j = 0x01; j < 0x09; j++ ) {
-        transmit(j, ((((uint8_t*)&adata.in_icon[0])[j - 1]) >> (adata.frame - (adata.space + ICON_W))) | 
-            ((((uint8_t*)&adata.in_icon[1])[j - 1]) << (adata.space + ICON_W + NUM_W - adata.frame)) |
-            ((((uint8_t*)&adata.in_icon[2])[j - 1]) << (adata.space + ICON_W + NUM_W + NUM_W - adata.frame)));
+        transmit(j, ((((uint8_t*)&tdata->in_icon[0])[j - 1]) >> (tdata->frame - (tdata->space + ICON_W))) | 
+            ((((uint8_t*)&tdata->in_icon[1])[j - 1]) << (tdata->space + ICON_W + NUM_W - tdata->frame)) |
+            ((((uint8_t*)&tdata->in_icon[2])[j - 1]) << (tdata->space + ICON_W + NUM_W + NUM_W - tdata->frame)));
       }
 
-    } else if( adata.frame <= adata.space + ICON_W + NUM_W + NUM_W
-        && adata.frame > adata.space + ICON_W + NUM_W
-        && adata.number > 99 ) {
+    } else if( tdata->frame <= tdata->space + ICON_W + NUM_W + NUM_W
+        && tdata->frame > tdata->space + ICON_W + NUM_W
+        && tdata->number > 99 ) {
       // wipe across the final NUM_W columns (clear second digit)
       for( uint8_t j = 0x01; j < 0x09; j++ ) {
-        transmit(j, ((((uint8_t*)&adata.in_icon[1])[j - 1]) >> (adata.frame - (adata.space + ICON_W + NUM_W))) | 
-            ((((uint8_t*)&adata.in_icon[2])[j - 1]) << (adata.space + ICON_W + NUM_W + NUM_W - adata.frame)));
+        transmit(j, ((((uint8_t*)&tdata->in_icon[1])[j - 1]) >> (tdata->frame - (tdata->space + ICON_W + NUM_W))) | 
+            ((((uint8_t*)&tdata->in_icon[2])[j - 1]) << (tdata->space + ICON_W + NUM_W + NUM_W - tdata->frame)));
       }
 
     } else {
       // this means that all of the digits have scrolled. Disable the timer and clean up
       TCCR0B &= (0xFF & !_BV(CS02) & !_BV(CS00));
-      adata.trans_done = true;
+      tdata->trans_done = true;
     }
   }
   return;
 }
 
+
+
 /* slide transition between icons
+ *  tdata:    pointer to the data struct to store the transition info in
  *  out_icon: old icon to be replaced
  *  in_icon:  new icon to be displayed
  *  space:    number of blank columns between icons */
-void icon_transition(const uint64_t out_icon, const uint64_t in_icon, const uint8_t space) {
-  adata.icon = true;
-  adata.out_icon = out_icon;
-  adata.in_icon[0] = in_icon;
-  adata.space = space;
+void icon_transition(transition_data* tdata,
+    const uint64_t out_icon,
+    const uint64_t in_icon,
+    const uint8_t space) {
+  tdata->icon = true;
+  tdata->out_icon = out_icon;
+  tdata->in_icon[0] = in_icon;
+  tdata->space = space;
 
   // kick off the first frame
-  adata.frame = 0;
-  transition_loop();
+  tdata->frame = 0;
+  transition_loop(tdata);
 
   /* set the timer value to zero before starting the clock to ensure we wait the full period
    * before triggering the second frame. /1024 prescaler on a 1MHz clock give a tick of ~1ms.
@@ -133,53 +144,46 @@ void icon_transition(const uint64_t out_icon, const uint64_t in_icon, const uint
   return;
 }
 
+
+
 /* slide transition to show a number. It will stop on the last number and return the 
  * index of that digit; this can be used to transition from that digit back to an icon.
+ *  tdata:    pointer to the data struct to store the transition info in
  *  out_icon: icon to be replaced
  *  number:   number to be displayed. This is passed as an integer that will be parsed.
  *  space:    number of blank columns between icon and number (0-7)
  * 
  * this is the nonblocking version of the above function */
 // <TODO> degree sign? Is this necessary?
-uint8_t number_transition(const uint64_t out_icon, const uint8_t number, const uint8_t space) {
-  adata.icon = false;
-  adata.out_icon = out_icon;
-  adata.space = space;
-  adata.number = number;
-  // parse digits in the number into the digit frame array
-  if( number > 99 ) {
-    adata.in_icon[0] = digit[number / 100];
-    adata.in_icon[1] = digit[(number / 10) % 10];
-    adata.in_icon[2] = digit[number % 10];
-  } else if ( number > 9 ) {
-    adata.in_icon[0] = digit[(number / 10) % 10];
-    adata.in_icon[1] = digit[number % 10];
-    adata.in_icon[2] = character[BLANK];
-  } else {
-    adata.in_icon[0] = digit[number % 10];
-    adata.in_icon[1] = character[BLANK];
-  }
-
+uint8_t number_transition(transition_data* tdata,
+    const uint64_t out_icon,
+    const uint8_t space) {
+  tdata->icon = false;
+  tdata->out_icon = out_icon;
+  tdata->space = space;
+  
   // kick off the first frame
-  adata.frame = 0;
-  transition_loop();
+  tdata->frame = 0;
+  transition_loop(tdata);
 
   /* set the timer value to zero before starting the clock to ensure we wait the full period
    * before triggering the second frame. /1024 prescaler on a 1MHz clock give a tick of ~1ms.
    * Poll the timer overflow flag in TIFR0 to determine when the timer has expired. */
   TCNT0 = 0x00;
   TCCR0B |= (_BV(CS02) | _BV(CS00));
-  return number % 10;
+  return tdata->number % 10;
 }
 
+
+
 // blocks until any currently running transitions are done
-void wait_for_transition(void) {
-  while( !adata.trans_done ) {
+void wait_for_transition(transition_data* tdata) {
+  while( !tdata->trans_done ) {
     if( TIFR0 & _BV(OCF0A) ) {
-      transition_loop();
+      transition_loop(tdata);
     }
   }
-  adata.trans_done = false;
+  tdata->trans_done = false;
 }
 
 #endif
--- main.c
+++ main.c
@@ -9,27 +9,31 @@
 #include <avr/io.h>
 // wait for a certain amount of time (for debugging and development)
 #include <util/delay.h>
-// allow constants to be stored in flash instead of SRAM
-#include <avr/pgmspace.h>
-// interrupts
-#include <avr/interrupt.h>
 // icon sets
 #include "graphics.h"
 // animation functions
 #include "animation.h"
 
-/* not used right now - may be used later to implement animated icons
-// update the entire matrix from a 64-bit number
-void update_screen(const uint64_t icon) {
-  // transmit 8 rows at a time. We're taking advantage of the fact that 
-  // the display registers start at 1 on the MAX72XX
-  // treat the icon like a uint8_t array
-  for( uint8_t i = 0x01; i < 0x09; i++ ) {
-    transmit(i, ((uint8_t*)&icon)[i - 1]);
+
+
+// take a 1, 2, or 3 digit number and parse it into three digits to fill transition_data.in_icon
+void num_to_icons(transition_data* tdata, uint8_t number) {
+  tdata->number = number;
+  if( number > 99 ) {
+    tdata->in_icon[0] = digit[number / 100];
+    tdata->in_icon[1] = digit[(number / 10) % 10];
+    tdata->in_icon[2] = digit[number % 10];
+  } else if ( number > 9 ) {
+    tdata->in_icon[0] = digit[(number / 10) % 10];
+    tdata->in_icon[1] = digit[number % 10];
+    tdata->in_icon[2] = character[BLANK];
+  } else {
+    tdata->in_icon[0] = digit[number % 10];
+    tdata->in_icon[1] = character[BLANK];
   }
-  return;
 }
-*/
+
+
 
 int main(void) {
   // turn off everything except the SPI interface and timer0
@@ -59,33 +63,39 @@ int main(void) {
   TCCR0A |= _BV(WGM01);
   TCCR0B &= (0xFF & !_BV(CS02) & !_BV(CS00));
   OCR0A = SLIDE_DELAY;
+  
+  // define a transition data storage struct
+  transition_data tdata;
 
   // slide the first icon onto the screen
-  icon_transition(character[BLANK], icon[FIRST], 0);
-  wait_for_transition();
+  icon_transition(&tdata, character[BLANK], icon[FIRST], 0);
+  wait_for_transition(&tdata);
   _delay_ms(2000);
 
   uint8_t last_digit = 0;
   while( 1 == 1 ) {
     // three digit number
-    last_digit = number_transition(icon[FIRST], 240, 3);
-    wait_for_transition();
-    icon_transition(digit[last_digit], icon[PRECIP], 2);
-    wait_for_transition();
+    num_to_icons(&tdata, 240);
+    last_digit = number_transition(&tdata, icon[FIRST], 3);
+    wait_for_transition(&tdata);
+    icon_transition(&tdata, digit[last_digit], icon[PRECIP], 2);
+    wait_for_transition(&tdata);
     _delay_ms(2000);
 
     // two digit number
-    last_digit = number_transition(icon[PRECIP], 36, 3);
-    wait_for_transition();
-    icon_transition(digit[last_digit], icon[SNOW], 2);
-    wait_for_transition();
+    num_to_icons(&tdata, 36);
+    last_digit = number_transition(&tdata, icon[PRECIP], 3);
+    wait_for_transition(&tdata);
+    icon_transition(&tdata, digit[last_digit], icon[SNOW], 2);
+    wait_for_transition(&tdata);
     _delay_ms(2000);
 
     // one digit number
-    last_digit = number_transition(icon[SNOW], 9, 3);
-    wait_for_transition();
-    icon_transition(digit[last_digit], icon[FIRST], 2);
-    wait_for_transition();
+    num_to_icons(&tdata, 9);
+    last_digit = number_transition(&tdata, icon[SNOW], 3);
+    wait_for_transition(&tdata);
+    icon_transition(&tdata, digit[last_digit], icon[FIRST], 2);
+    wait_for_transition(&tdata);
     _delay_ms(2000);
   }
 }
