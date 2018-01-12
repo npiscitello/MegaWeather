ece114e23714c227f48ed8e48672b363f23c897b
Nick Piscitello <npiscitello@users.noreply.github.com>
2018-01-11 21:03:39 -0500

    fixed Makefile bug, added janky animation as proof of concept

--- src/main.c
+++ src/main.c
@@ -1,26 +1,36 @@
 #include "ets_sys.h"
 #include "osapi.h"
 #include "driver/spi.h"
+#include "graphics.h"
 
 // transmit a register/data pair
 #define spi_transmit(ADDR, DATA) spi_transaction(HSPI, 0, 0, 8, ADDR, 8, DATA, 0, 0);
 
 // blink timers
-static volatile os_timer_t on_timer;
-static volatile os_timer_t off_timer;
-#define PERIOD 2000
-#define ON_TIME 1000
+static volatile os_timer_t trans_timer;
+#define PERIOD 250
 
-void ICACHE_FLASH_ATTR led_off(void *arg) {
-  (void)arg;
-  spi_transmit(0x0F, 0x00);
-  os_timer_disarm(&off_timer);
-}
+uint8_t counter = 0x01;
+uint8_t flip = 0;
 
-void ICACHE_FLASH_ATTR led_on(void *arg) {
+void ICACHE_FLASH_ATTR disp_image(void *arg) {
   (void)arg;
-  spi_transmit(0x0F, 0x01);
-  os_timer_arm(&off_timer, ON_TIME, 1);
+  for( counter = 0x01; counter <= 0x08; counter++ ) {
+    if( counter % 2 == 0 ) {
+      if( flip == 0 ) {
+        spi_transmit(counter, 0xaa);
+      } else {
+        spi_transmit(counter, 0x55);
+      }
+    } else {
+      if( flip == 0 ) {
+        spi_transmit(counter, 0x55);
+      } else {
+        spi_transmit(counter, 0xaa);
+      }
+    }
+  }
+  flip = ~flip;
 }
 
 void ICACHE_FLASH_ATTR user_init()
@@ -38,16 +48,15 @@ void ICACHE_FLASH_ATTR user_init()
   spi_transmit(0x0A, 0x08);
   // scan across all digits
   spi_transmit(0x0b, 0x07);
-  // take the chip out of shutdown
-  spi_transmit(0x0C, 0x01);
   // turn off all pixels
-  for( uint8_t i = 0x01; i < 0x08; i++ ) {
+  for( uint8_t i = 0x01; i <= 0x08; i++ ) {
     spi_transmit(i, 0x00);
   }
+  // take the chip out of shutdown
+  spi_transmit(0x0C, 0x01);
 
   // setup timers
-  os_timer_setfn(&on_timer, (os_timer_func_t *)led_on, NULL);
-  os_timer_setfn(&off_timer, (os_timer_func_t *)led_off, NULL);
-  os_timer_arm(&on_timer, PERIOD, 1);
+  os_timer_setfn(&trans_timer, (os_timer_func_t *)disp_image, NULL);
+  os_timer_arm(&trans_timer, PERIOD, 1);
 
 }
